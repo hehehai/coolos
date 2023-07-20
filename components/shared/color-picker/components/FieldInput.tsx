467b9712ed0f5f3855ba99373a0ae650cd7c0baf
hehehai <riverhohai@gmail.com>
2023-07-20 12:24:09 +0800

    feat(color-picker): field input

---
+++ components/shared/color-picker/components/FieldInput.tsx
@@ -0,0 +1,70 @@
+import {useEffect, useState} from "react";
+import {cn} from "@/lib/utils";
+
+export interface FieldInputProps extends Omit<React.ComponentPropsWithoutRef<'input'>, 'onChange'> {
+    value?: number;
+    title: string;
+    min?: number;
+    max?: number;
+    onChange?: (val: number) => void
+}
+
+const FieldInput = ({
+                        title,
+                        value = 0,
+                        min = 0,
+                        max = 100,
+                        onChange,
+                        children,
+
+                        ...rest
+                    }: FieldInputProps) => {
+    const [tmpVal, setTmpVal] = useState<number | string>(Math.trunc(value))
+
+    useEffect(() => {
+        setTmpVal(Math.trunc(value))
+    }, [value])
+
+    const handleChange = (e: React.SyntheticEvent) => {
+        const val = (e.target as HTMLInputElement).value
+        setTmpVal(val)
+    }
+
+    const handleBlur = (e: React.SyntheticEvent) => {
+        const val = (e.target as HTMLInputElement).value
+        if (val === '') {
+            onChange?.(0)
+        } else if (/[-+]?d*/.test(val)) {
+            const newVal = Math.abs(Number.parseInt(val))
+            // FIX: 舍去小数，未重新赋值
+            setTmpVal(newVal)
+            onChange?.(newVal)
+        } else {
+            setTmpVal(Math.trunc(value))
+        }
+    }
+
+    return <div className={'space-y-2'}>
+        <div className={'flex items-center justify-between'}>
+            <label className={'text-sm'}>{title}</label>
+            <div>
+                <input
+                    className={cn('number-hide-arrows w-12 px-[5px] h-6 text-xs rounded-md border border-gray-300 text-center border-transparent hover:border-gray-300 focus:border-blue-500')}
+                    type="number"
+                    value={tmpVal}
+                    min={min}
+                    max={max}
+                    pattern="[-+]?d*"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    {...rest}
+                />
+            </div>
+        </div>
+        {children}
+    </div>
+}
+
+FieldInput.displayName = 'FieldInput'
+
+export default FieldInput
