a84b8d511781c22e2f5cb9e732d640d375f07fa6
hehehai <riverhohai@gmail.com>
2023-08-30 10:29:56 +0800

    feat: full screen pop animation

--- components/shared/color-fullscreen/index.tsx
+++ components/shared/color-fullscreen/index.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
+import { AnimatePresence, motion } from 'framer-motion'
+
+const fullScreenMotion = {
+  visible: { y: 0 },
+  hidden: { y: '100%' },
+}
 
 const ColorFullScreen = (props: {
   show: boolean;
@@ -8,14 +14,21 @@ const ColorFullScreen = (props: {
 }) => {
   const { show, color, onClose } = props
 
-  if (!show) return null;
-
-  return <div className="fixed inset-0 z-[999]">
-    <Button className="absolute right-4 top-4 z-20" variant="outline" size="icon" onClick={() => onClose?.()}>
-      <X className="h-4 w-4" />
-    </Button>
-    <div className="absolute insert-0 z-10 w-full h-full" style={{ backgroundColor: color }} />
-  </div>
+  return <AnimatePresence>
+    {show && <motion.div
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      variants={fullScreenMotion}
+      transition={{ ease: 'easeIn', duration: 0.2 }}
+      className="fixed inset-0 z-[999]"
+    >
+      <Button className="absolute right-4 top-4 z-20" variant="outline" size="icon" onClick={() => onClose?.()}>
+        <X className="h-4 w-4" />
+      </Button>
+      <div className="absolute insert-0 z-10 w-full h-full" style={{ backgroundColor: color }} />
+    </motion.div>}
+  </AnimatePresence>
 }
 
 ColorFullScreen.displayName = "ColorFullScreen";
