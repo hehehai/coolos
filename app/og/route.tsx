8123e95efd0d896f488bbb48aaeaf255ed445d11
hehehai <riverhohai@gmail.com>
2023-09-07 21:36:04 +0800

    feat: site og img

---
+++ app/og/route.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from 'next/server';
+// App router includes @vercel/og.
+// No need to install it.
+
+export const runtime = 'edge';
+
+export async function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 50,
+          color: 'black',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        👋, 🌎
+      </div>
+    ),
+    {
+      width: 600,
+      height: 430,
+      // Supported options: 'twemoji', 'blobmoji', 'noto', 'openmoji', 'fluent' and 'fluentFlat'
+      // Default to 'twemoji'
+      emoji: 'twemoji',
+    },
+  );
+}
