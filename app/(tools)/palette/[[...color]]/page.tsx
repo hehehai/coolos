d76988520897cc20dfcf1e8553addd93ac4a2617
hehehai <riverhohai@gmail.com>
2023-09-16 10:32:17 +0800

    chore: wip color palette page

---
+++ app/(tools)/palette/[[...color]]/page.tsx
@@ -0,0 +1,10 @@
+const ColorPalette = () => {
+  // 获取路由的参数
+  // 色板块颜色参数
+
+  // 渲染色板操作条
+  // 渲染颜色块
+  return <div>ColorPalette</div>
+}
+
+export default ColorPalette
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import { Toaster } from "react-hot-toast"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Coolors",
+  title: "Coolos",
   description: "fast color generator",
 }
 
