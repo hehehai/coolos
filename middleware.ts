445139133cec8e4edb8c3f288b0c2bc5cbf2e262
hehehai <riverhohai@gmail.com>
2023-09-28 10:13:52 +0800

    fix: clerk middleware error

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,21 @@
-import { authMiddleware } from "@clerk/nextjs"
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs"
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({})
+export default authMiddleware({
+  debug: process.env.NODE_ENV === "development",
+  publicRoutes: [
+    "/",
+    "/(colors|contrast|picker|explore-palette|gradient-palette|palette)(.*)",
+  ],
+  afterAuth(auth, req, _evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url })
+    }
+  },
+})
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
