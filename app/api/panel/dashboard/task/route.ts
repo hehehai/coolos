3f19aee2abc4eb2171fe667519149bcb538cb98b
hehehai <riverhohai@gmail.com>
2023-11-18 22:31:13 +0800

    fix: ts

--- app/api/panel/dashboard/task/route.ts
+++ app/api/panel/dashboard/task/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 import { auth, currentUser } from "@clerk/nextjs"
 
 import { cronTask } from "@/lib/cron-task"
